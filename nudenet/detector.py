7a752ab37acf5dc26b39af604ebeab17daa21214
heisen273 <f0xkid@ukr.net>
2020-06-19 09:42:08 +0200

    Update detector.py

--- nudenet/detector.py
+++ nudenet/detector.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow.keras as keras
 import pydload
-from tensorflow.keras import models
+from keras_retinanet import models
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
 from keras_retinanet.utils.visualization import draw_box, draw_caption
 from keras_retinanet.utils.colors import label_color
